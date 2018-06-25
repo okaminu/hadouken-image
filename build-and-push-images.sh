d1f3aa75d291d20f00ff4a1d407cfe97241fd6e8
Okaminu <aurimasdgt@gmail.com>
2018-06-25 18:34:12 +0300

    BA-596: buildspec for CodeBuild to build and push docker images

---
+++ build-and-push-images.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+docker login -u bolddevs -p kotletasserveryje
+
+docker image build -t boldadmin/http-runner-from-s3:nginx1.12 ./http-runner-from-s3
+docker push boldadmin/http-runner-from-s3:nginx1.12
+
+docker image build -t boldadmin/jar-runner-from-s3:jre8u151 ./jar-runner-from-s3/
+docker push boldadmin/jar-runner-from-s3:jre8u151
+
+docker image build -t boldadmin/mongodb-seeder-from-git:1.2 ./mongodb-seeder-from-git/
+docker push boldadmin/mongodb-seeder-from-git:1.2
