037cd1be1338091a251e667d0f3138e2907ffe9a
Okaminu <aurimasdgt@gmail.com>
2018-07-03 20:48:57 +0300

    BA-596: prometheus as a monitoring tool

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -3,7 +3,7 @@
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker image build -t boldadmin/http-runner-from-s3:nginx1.12 ./http-runner-from-s3
+docker image build -t boldadmin/http-runner-from-s3:nginx1.12 ./http-runner-from-s3/
 docker push boldadmin/http-runner-from-s3:nginx1.12
 
 docker image build -t boldadmin/jar-runner-from-s3:jre8u151 ./jar-runner-from-s3/
@@ -11,3 +11,6 @@ docker push boldadmin/jar-runner-from-s3:jre8u151
 
 docker image build -t boldadmin/mongodb-seeder-from-git:1.2 ./mongodb-seeder-from-git/
 docker push boldadmin/mongodb-seeder-from-git:1.2
+
+docker image build -t boldadmin/prometheus:v2.3.1 ./prometheus/
+docker push boldadmin/prometheus:v2.3.1
