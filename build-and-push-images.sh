425488f69ab97277eb641111a20ba9decc7cadcf
vsh1ft <kastytis.ch@gmail.com>
2018-09-24 11:17:08 +0300

    BA-499: database seeder clones from a specific branch

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -9,8 +9,8 @@ docker push boldadmin/http-runner-from-s3:nginx1.12
 docker image build -t boldadmin/jar-runner-from-s3:jre8u151 ./jar-runner-from-s3/
 docker push boldadmin/jar-runner-from-s3:jre8u151
 
-docker image build -t boldadmin/mongodb-seeder-from-git:1.2 ./mongodb-seeder-from-git/
-docker push boldadmin/mongodb-seeder-from-git:1.2
+docker image build -t boldadmin/mongodb-seeder-from-git:1.3 ./mongodb-seeder-from-git/
+docker push boldadmin/mongodb-seeder-from-git:1.3
 
 docker image build -t boldadmin/prometheus:v2.3.1 ./prometheus/
 docker push boldadmin/prometheus:v2.3.1
