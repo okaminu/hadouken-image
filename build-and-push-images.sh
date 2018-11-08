698819b9bfd35d680f56b940d2729c498e4a4e5d
Okaminu <aurimasdgt@gmail.com>
2018-11-08 12:00:32 +0200

    updated java version from 8 to 11 for jar runner

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -6,8 +6,8 @@ docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker image build -t boldadmin/http-runner-from-s3:nginx1.12 ./http-runner-from-s3/
 docker push boldadmin/http-runner-from-s3:nginx1.12
 
-docker image build -t boldadmin/jar-runner-from-s3:jre8u151 ./jar-runner-from-s3/
-docker push boldadmin/jar-runner-from-s3:jre8u151
+docker image build -t boldadmin/jar-runner-from-s3:11.0.2-jre ./jar-runner-from-s3/
+docker push boldadmin/jar-runner-from-s3:11.0.2-jre
 
 docker image build -t boldadmin/mongodb-seeder-from-git:1.3 ./mongodb-seeder-from-git/
 docker push boldadmin/mongodb-seeder-from-git:1.3
