b9c815e025029260e36c51b3cab99d1a66f5d729
Andrius Degutis <andriusdgt@gmail.com>
2019-06-20 17:01:14 +0300

    BA-901: nginx, openjdk, prometheus versions increased

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -3,14 +3,14 @@
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker image build -t boldadmin/http-runner-from-s3:nginx1.14 ./http-runner-from-s3/
-docker push boldadmin/http-runner-from-s3:nginx1.14
+docker image build -t boldadmin/http-runner-from-s3:nginx1.16 ./http-runner-from-s3/
+docker push boldadmin/http-runner-from-s3:nginx1.16
 
-docker image build -t boldadmin/jar-runner-from-s3:11.0.1-jre ./jar-runner-from-s3/
-docker push boldadmin/jar-runner-from-s3:11.0.1-jre
+docker image build -t boldadmin/jar-runner-from-s3:11.0.3-jre ./jar-runner-from-s3/
+docker push boldadmin/jar-runner-from-s3:11.0.3-jre
 
 docker image build -t boldadmin/mongodb-seeder-from-git:1.3 ./mongodb-seeder-from-git/
 docker push boldadmin/mongodb-seeder-from-git:1.3
 
-docker image build -t boldadmin/prometheus:v2.3.1 ./prometheus/
-docker push boldadmin/prometheus:v2.3.1
+docker image build -t boldadmin/prometheus:v2.10.0 ./prometheus/
+docker push boldadmin/prometheus:v2.10.0
