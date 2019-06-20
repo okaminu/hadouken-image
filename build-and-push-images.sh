27f0bad24db0453ffa91e694c890a77853ad3df5
Andrius Degutis <andriusdgt@gmail.com>
2019-06-20 18:27:05 +0300

    BA-901: increased mongodb seeder image version

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -9,8 +9,8 @@ docker push boldadmin/http-runner-from-s3:nginx1.16
 docker image build -t boldadmin/jar-runner-from-s3:11.0.3-jre ./jar-runner-from-s3/
 docker push boldadmin/jar-runner-from-s3:11.0.3-jre
 
-docker image build -t boldadmin/mongodb-seeder-from-git:1.3 ./mongodb-seeder-from-git/
-docker push boldadmin/mongodb-seeder-from-git:1.3
+docker image build -t boldadmin/mongodb-seeder-from-git:1.4 ./mongodb-seeder-from-git/
+docker push boldadmin/mongodb-seeder-from-git:1.4
 
 docker image build -t boldadmin/prometheus:v2.10.0 ./prometheus/
 docker push boldadmin/prometheus:v2.10.0
