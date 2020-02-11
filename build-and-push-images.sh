915880e07f930d7ff3152ba4e783c35ab05becff
Okaminu <aurimasdgt@gmail.com>
2020-02-11 18:50:25 +0200

    java max heap sizes for services

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -6,8 +6,8 @@ docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker image build -t boldadmin/http-runner-from-s3:nginx1.17.5 ./http-runner-from-s3/
 docker push boldadmin/http-runner-from-s3:nginx1.17.5
 
-docker image build -t boldadmin/jar-runner-from-s3:11.0.3-jre-trim ./jar-runner-from-s3/
-docker push boldadmin/jar-runner-from-s3:11.0.3-jre-trim
+docker image build -t boldadmin/jar-runner-from-s3:11.0.4 ./jar-runner-from-s3/
+docker push boldadmin/jar-runner-from-s3:11.0.4
 
 docker image build -t boldadmin/mongodb-seeder-from-git:1.5 ./mongodb-seeder-from-git/
 docker push boldadmin/mongodb-seeder-from-git:1.5
--- jar-runner-from-s3/scripts/launch-jar.sh
+++ jar-runner-from-s3/scripts/launch-jar.sh
@@ -4,7 +4,7 @@ aws s3 cp $APP_URL ./app.jar
 
 if [ "$SSL" = true ]
 then
-    java -jar ./app.jar --server.port=8443 --server.ssl.key-store=/srv/cert.p12 --server.ssl.key-store-password=$KEYSTORE_PASS
+    java -Xmx$JVM_HEAP_MAX_SIZE -jar ./app.jar --server.port=8443 --server.ssl.key-store=/srv/cert.p12 --server.ssl.key-store-password=$KEYSTORE_PASS
 else
-    java -jar ./app.jar
+    java -Xmx$JVM_HEAP_MAX_SIZE -jar ./app.jar
 fi
