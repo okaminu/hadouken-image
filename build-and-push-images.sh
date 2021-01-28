616be0c9b6f2ce2439ea1e49ac9fd7b3057aaaa6
Okaminu <aurimasdgt@gmail.com>
2021-01-28 23:46:53 +0200

    updated keys and enabled no-ssl mode for prod env, upgraded jdk images in order to support newer awscli, since older are deprecated

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -6,11 +6,11 @@ docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker image build -t boldadmin1/http-runner-from-s3:nginx1.17.5 ./http-runner-from-s3/
 docker push boldadmin1/http-runner-from-s3:nginx1.17.5
 
-docker image build -t boldadmin1/jar-runner-from-s3:11.0.4 ./jar-runner-from-s3/
-docker push boldadmin1/jar-runner-from-s3:11.0.4
+docker image build -t boldadmin1/jar-runner-from-s3:11.0.9 ./jar-runner-from-s3/
+docker push boldadmin1/jar-runner-from-s3:11.0.9
 
-docker image build -t boldadmin1/mongodb-seeder-from-git:1.5 ./mongodb-seeder-from-git/
-docker push boldadmin1/mongodb-seeder-from-git:1.5
+docker image build -t boldadmin1/mongodb-seeder-from-git:1.7 ./mongodb-seeder-from-git/
+docker push boldadmin1/mongodb-seeder-from-git:1.7
 
 docker image build -t boldadmin1/prometheus:v2.13.1 ./prometheus/
 docker push boldadmin1/prometheus:v2.13.1
--- mongodb-seeder-from-git/checkout-git.sh
+++ mongodb-seeder-from-git/checkout-git.sh
@@ -2,11 +2,9 @@
 
 mkdir -p /root/.ssh/
 
-echo -e $SSH_CONFIG > /root/.ssh/config
-
 echo -e $SSH_PRIVATE_KEY > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 
 ssh-keyscan $REPOSITORY_PROVIDER_URL >> /root/.ssh/known_hosts
 
-git clone -b $BRANCH $COMPLETE_REPOSITORY_URL$REPOSITORY_NAME
+git clone -b $BRANCH $COMPLETE_REPOSITORY_URL$REPOSITORY_NAME.git
